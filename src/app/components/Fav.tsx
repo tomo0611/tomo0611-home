c65a3c50051d571a4b4db4d0794fee7fa3aef991
tomo0611 <tomo0611@hotmail.com>
2023-08-23 10:37:27 +0900

    アイコンの中央寄せ

--- src/app/components/Fav.tsx
+++ src/app/components/Fav.tsx
@@ -7,12 +7,11 @@ export function Fav({ navLinks }: { navLinks: { name: string, href: string, icon
             {navLinks.map((link) => {
                 return (
                     <Link
-                        className={'w-12 h-12 bg-gray-200 hover:bg-pink-300 rounded-full duration-200 m-2 p-4 text-center leading-4 min-h-12'}
-                        href={link.href}
                         key={link.name}
-                        target='_blank'
-                    >
-                        <i className={"w-12 h-12 text-primary si si--color si-" + link.icon}></i>
+                        className='w-12 h-12 bg-gray-200 hover:bg-pink-300 rounded-full duration-200 m-2 text-center'
+                        href={link.href}
+                        target='_blank'>
+                            <i className={"pt-4 text-xl si si--color si-" + link.icon}></i>
                     </Link>
                 )
             })}
