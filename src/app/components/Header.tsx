33df2da1f98ffb18f641a150e7c1e50e942f7f32
tomo0611 <tomo0611@hotmail.com>
2023-08-23 11:07:11 +0900

    ヘッダーのホバー処理しっかりできた！

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,10 +3,12 @@ import Link from 'next/link'
 
 export function Header() {
     return (
-        <header className='bg-white'>
-            <div className='flex items-center px-3 py-4'>
-                <Image className="rounded-full" src="/img/avatar.png" alt="tomo0611's avatar" width={30} height={30}/>
-                <span className='ml-3 font-bold'>tomo0611.jp</span>
+        <header className='bg-white shadow'>
+            <div className='max-w-5xl mx-auto'>
+                <Link href='/' className='items-center cursor-pointer hover:bg-red-100 duration-200 px-3 py-4 inline-flex'>
+                    <Image className="rounded-full" src="/img/avatar.png" alt="tomo0611's avatar" width={36} height={36} />
+                    <span className='ml-3 font-bold'>tomo0611.jp</span>
+                </Link>
             </div>
         </header>
     )
