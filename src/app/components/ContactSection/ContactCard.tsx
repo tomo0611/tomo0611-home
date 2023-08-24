994bab52c6139c42584eabba297aebd3d4d3d724
tomo0611 <tomo0611@hotmail.com>
2023-08-25 00:03:11 +0900

    ContactCardの横幅を修正

--- src/app/components/ContactSection/ContactCard.tsx
+++ src/app/components/ContactSection/ContactCard.tsx
@@ -4,11 +4,11 @@ import Link from "next/link";
 export function ContactCard({ contact }: { contact: contact }) {
     return (
         <Link className="bg-white drop-shadow rounded-lg hover:drop-shadow-lg duration-200" href={contact.url} target="_blank">
-            <div className="flex gap-4 mt-3 ml-2">
+            <div className="flex gap-4 ml-2">
 
-                <i className={"mx-4 mt-5 text-xl si si--color si-" + contact.icon_name}></i>
+                <i className={"p-4 h-full my-auto text-2xl si si--color si-" + contact.icon_name}></i>
 
-                <div className="mb-4">
+                <div className="mb-4 mt-3">
                     <p className="font-semibold">{contact.name}</p>
                     <p><span className="inline-block">{contact.text1}</span><span className="inline-block">{contact.text2}</span></p>
                 </div>
