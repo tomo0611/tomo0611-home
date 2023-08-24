840651768e06869f546714e3c375121d06f846a3
tomo0611 <tomo0611@hotmail.com>
2023-08-24 11:22:28 +0900

    Skillを少し更新

--- src/app/data/skills.tsx
+++ src/app/data/skills.tsx
@@ -14,13 +14,16 @@ export const skills: skill[] = [
         name: 'Visual Studio Code', icon_name: 'vscode', category: 'Software', text1: '一番使ってる開発環境。', text2: ''
     },
     {
-        name: 'GitHub & Git', icon_name: 'github', category: 'Service & Version Control Software', text1: 'いつも使ってます。', text2: ''
+        name: 'GitHub & Git', icon_name: 'github', category: 'Service & Version Control Software', text1: 'いつも使ってます。', text2: '職場にも導入してもらった'
     },
     {
-        name: 'Linux (Ubuntu)', icon_name: 'linux', category: 'Operating System', text1: '職場でRHELを強要されてます、', text2: 'タスケテ、、、'
+        name: 'Linux (Ubuntu)', icon_name: 'linux', category: 'Operating System', text1: '職場でRHELを強要されかけた', text2: 'アブナイ、、、'
     },
     {
         name: 'Docker', icon_name: 'docker', category: 'Containerization', text1: '部活の大先輩に言われて始めました。', text2: ''
+    },
+    {
+        name: 'TypeScript', icon_name: 'typescript', category: 'Language', text1: '型付けがしっかりしてて', text2: '安心❗'
     },
     {
         name: 'C++', icon_name: 'cpp', category: 'Language', text1: '競プロのために始めました。', text2: ''
@@ -29,7 +32,7 @@ export const skills: skill[] = [
         name: 'Python', icon_name: 'py', category: 'Language', text1: 'スクレイピングとかで使う。', text2: ''
     },
     {
-        name: 'Kotlin', icon_name: 'kotlin', category: 'language', text1: 'Androidアプリ開発用。', text2: ''
+        name: 'Kotlin', icon_name: 'kotlin', category: 'Language', text1: 'Androidアプリ開発用。', text2: ''
     },
     {
         name: 'Android Studio', icon_name: 'androidstudio', category: 'Software', text1: '小6からやってます。', text2: ''
@@ -38,15 +41,12 @@ export const skills: skill[] = [
         name: 'Figma', icon_name: 'figma', category: 'Software', text1: 'まだまだ勉強中❗', text2: ''
     },
     {
-        name: 'Firebase', icon_name: 'firebase', category: 'Cloud Platform', text1: '今はHostingを主に利用。', text2: '昔はAndroidアプリで使ってた。'
+        name: 'Firebase', icon_name: 'firebase', category: 'Cloud Platform', text1: '今はHostingを主に利用。', text2: 'Androidアプリでも使ってる。'
     },
     {
         name: 'Photoshop', icon_name: 'ps', category: 'Software', text1: '画像編集用。', text2: ''
     },
     {
         name: 'PHP', icon_name: 'php', category: 'Language', text1: '最近はもうやってない。', text2: ''
-    },
-    {
-        name: '正規表現', icon_name: 'regex', category: 'Language', text1: 'しっかりやりたい', text2: ''
     }
 ]
