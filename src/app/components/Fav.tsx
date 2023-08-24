0b44f2b4710eb8f80d248e5d664d047eee0eb417
tomo0611 <tomo0611@hotmail.com>
2023-08-25 00:15:26 +0900

    Favを調整

--- src/app/components/Fav.tsx
+++ src/app/components/Fav.tsx
@@ -8,7 +8,7 @@ export function Fav({ navLinks }: { navLinks: { name: string, href: string, icon
                 return (
                     <Link
                         key={link.name}
-                        className='m-1 rounded-full text-center'
+                        className='m-2 rounded-full text-center'
                         href={link.href}
                         target='_blank'>
                             <i className={"text-xl p-3 rounded-full hover:bg-pink-300 duration-200 bg-gray-200 si si--color si-" + link.icon}></i>
