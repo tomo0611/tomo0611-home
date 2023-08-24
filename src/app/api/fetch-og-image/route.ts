92a0863741f1fa0dd9f387174117b4155490dc2d
tomo0611 <tomo0611@hotmail.com>
2023-08-24 12:07:28 +0900

    OG Image取得APIの作成

---
+++ src/app/api/fetch-og-image/route.ts
@@ -0,0 +1,17 @@
+export async function GET(request: Request) {
+    const { searchParams } = new URL(request.url);
+
+    const hasUrl = searchParams.has("url");
+    const url = hasUrl
+        ? searchParams.get("url")!!
+        : "https://ch-random.net/post/461/featured.png";
+
+    const imageRes = await fetch(url);
+    const blob = await imageRes.blob();
+    const arrayBuffer = await blob.arrayBuffer();
+    const contentType = imageRes.headers.get('Content-Type');
+
+    // https://stackoverflow.com/questions/63066985/send-file-as-response-using-nextjs-api
+
+    return new Response(Buffer.from(arrayBuffer), { headers: { 'content-type': contentType ?? 'image/png' } });
+}
