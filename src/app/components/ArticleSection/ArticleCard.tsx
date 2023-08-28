ec0be036d7fbd2657fffc684f1c697407a21d124
tomo0611 <tomo0611@hotmail.com>
2023-08-28 14:58:13 +0900

    記事のアイコンを丸く

--- src/app/components/ArticleSection/ArticleCard.tsx
+++ src/app/components/ArticleSection/ArticleCard.tsx
@@ -10,7 +10,7 @@ type ArticleCardProps = {
 export const ArticleCard: FC<ArticleCardProps> = ({ article }) => {
     return <Link className="bg-white p-3 drop-shadow rounded-lg hover:drop-shadow-lg duration-200 w-[18rem] flex-inline" href={article.url} target='_blank'>
         <div className='relative w-18 h-40'>
-            <Image className="object-cover" src={`/api/fetch-og-image?url=${encodeURIComponent(article.thumbnailPath)}`} alt={article.title} fill />
+            <Image className="object-cover rounded-md" src={`/api/fetch-og-image?url=${encodeURIComponent(article.thumbnailPath)}`} alt={article.title} fill />
         </div>
         <div className='mt-3 border' />
         <p className='font-semibold mt-2'>{article.title}</p>
