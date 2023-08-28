97ca8bb8ed4396f6da8b6424e4cd04064c8451b0
tomo0611 <tomo0611@hotmail.com>
2023-08-28 14:52:21 +0900

    randomからの記事取得をできるように

--- src/app/api/articles/route.ts
+++ src/app/api/articles/route.ts
@@ -1,5 +1,6 @@
-import { random_article_response } from "@/app/data/articles";
+import { article, random_article_response } from "@/app/data/articles";
 import { NextResponse } from "next/server";
+import { parse } from 'node-html-parser';
 
 export async function GET(request: Request) {
     const { searchParams } = new URL(request.url);
@@ -9,23 +10,26 @@ export async function GET(request: Request) {
         ? searchParams.get("url")!!
         : "https://ch-random.net/post/461/featured.png";
 
-    const articles_random = await (await fetch("https://ch-random.net/_next/data/csd1zxkbkdzrm1NyApXBF/authors/tomo.json?username=tomo")).json();
+    const res = (await fetch("https://ch-random.net/authors/tomo/")).text();
+    const root = parse(await res);
+    const articles_random = JSON.parse(root.querySelector("#__NEXT_DATA__")!!.innerHTML).props;
 
     const articles_random_data = (articles_random as random_article_response).pageProps.posts;
 
-    const articles = [];
+    const articles: article[] = [];
 
-    for(let i=0; i<articles_random_data.length; i++){
+    for (let i = 0; i < articles_random_data.length; i++) {
         const article = articles_random_data[i];
         articles.push({
             id: article.id,
             title: article.title,
-            created_at: article.lastmod.substring(0,10).replaceAll("-", "/"),
+            created_at: article.lastmod.substring(0, 10).replaceAll("-", "/"),
             url: `https://ch-random.net/post/${article.id}/`,
             thumbnailPath: `https://ch-random.net${article.thumbnailPath}`,
             media: "random"
         })
     }
+
     // 今後書いたらZennも追加する
     // https://zenn.dev/api/articles?username=tomo0611&order=latest
     return NextResponse.json(articles);
